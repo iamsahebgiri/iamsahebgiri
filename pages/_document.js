a041b42a114c4746992058ae880a871d15376cc5
iamsahebgiri <iamsahebgiri@gmail.com>
2021-01-02 17:21:33 +0530

    possible fix

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,34 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+import { themeStorageKey } from "./../lib/useTheme";
+
+const bgVariableName = "--bg";
 
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html lang='en'>
-        <Head />
+      <Html lang="en">
+        <Head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function() {
+                try {
+                  var outdatedValue = localStorage.getItem('light-mode')
+                  if (outdatedValue) {
+                    localStorage.setItem('${themeStorageKey}', 'light')
+                    localStorage.removeItem('light-mode')
+                  }
+                  var mode = localStorage.getItem('${themeStorageKey}')
+                  if (!mode) return
+                  document.documentElement.classList.add(mode)
+                  var bgValue = getComputedStyle(document.documentElement)
+                    .getPropertyValue('${bgVariableName}')
+                  document.documentElement.style.background = bgValue
+                } catch (e) {}
+              })()`,
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
