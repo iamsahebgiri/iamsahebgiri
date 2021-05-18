41efb84e63310a11f3120128967696802a020aa2
Saheb Giri <iamsahebgiri>
2021-05-18 23:16:15 +0530

    👌 IMPROVE: Add Hero component

---
+++ components/Hero.js
@@ -0,0 +1,32 @@
+import Link from 'next/link';
+import React from 'react';
+import { Button, Heading, Flex, Text, HStack } from '@chakra-ui/react';
+
+export default function Intro() {
+  return (
+    <Flex flexDir="column" alignItems="center" py={[20]}>
+      <Heading mb={3} as="h1">
+        Hey, I am
+        <span className="text-gradient">{' Saheb Giri'}</span>
+      </Heading>
+      <Text textAlign="center" maxW="xl" color="gray.600">
+        I’m a developer and a student reading CSIT in SOA University at
+        Bhubaneswar. You’ve found my personal slice of the internet.
+      </Text>
+      <HStack mt={6} spacing={3} zIndex={0}>
+        <Link href="/about">
+          <a>
+            <Button colorScheme="orange">More about me</Button>
+          </a>
+        </Link>
+        <a
+          target="_blank"
+          rel="noopener noreferer"
+          href="https://docs.google.com/document/d/135XdFRBiKadLyHH2k5wN9fanflPzGwHmhIHwviUxay8/edit?usp=sharing"
+        >
+          <Button>Resume</Button>
+        </a>
+      </HStack>
+    </Flex>
+  );
+}
