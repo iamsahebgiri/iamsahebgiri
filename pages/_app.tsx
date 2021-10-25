9db74d5c5565ec07ea76422dc8330a6d2629327e
Saheb Giri <iamsahebgiri@gmail.com>
2021-10-26 01:04:25 +0530

    fix: add ibm plex mono font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+import '@fontsource/ibm-plex-mono/400.css';
+import '@fontsource/ibm-plex-mono/400-italic.css';
 import customTheme from 'theme';
 
 import 'styles/index.css';
--- theme/foundations/typography.ts
+++ theme/foundations/typography.ts
@@ -2,7 +2,7 @@ const typography = {
   fonts: {
     heading: `"Inter",-apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"`,
     body: `"Inter",-apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"`,
-    mono: `"Roboto Mono",Menlo,Monaco,Consolas,"Liberation Mono","Courier New",monospace;`
+    mono: `"IBM Plex Mono",Menlo,Monaco,Consolas,"Liberation Mono","Courier New",monospace;`
   }
 };
 
