1192427873eb82191e577f2bfdc9a7254a55e058
Saheb Giri <iamsahebgiri@gmail.com>
2023-07-06 14:10:30 +0530

    chore: using nginx as load balancer and docker compose

--- utils/create-post.js
+++ utils/create-post.js
@@ -1,46 +1,56 @@
-const fs = require("fs");
-
+const fs = require('fs');
 const args = process.argv.slice(2);
 
-if (args.length != 1) {
-  console.error("error: missing argument");
-  console.error("usage: create-post <file_name>");
-  return;
-}
+(() => {
+  if (args.length != 1) {
+    console.error('error: missing argument');
+    console.error('usage: npm run post <title>');
+    return;
+  }
 
-console.log("Started writing...");
+  console.log('Started writing...');
 
-const markdownFileName = `./_posts/${args[0]}.md`;
-const folderName = `./public/assets/blog/${args[0]}`;
-const content = 
-`---
-title: ''
-excerpt: ''
-coverImage: '/assets/blog/${args[0]}/cover.jpg'
-date: '${new Date()}'
-type: ''
-author:
-  name: Saheb Giri
-  picture: '/assets/blog/authors/sg.png'
-ogImage:
-  url: '/assets/blog/${args[0]}/cover.jpg'
+  const title = args[0];
+  const slug = sluggify(title);
+  const path = `./data/overthoughts/${slug}.mdx`;
+  const imagePath = `/assets/overthoughts/${slug}`;
+  const content = `---
+title: '${title}'
+summary: 'Summary'
+publishedAt: '${new Date().toISOString().slice(0, 10)}'
+image: '${imagePath}/cover.jpg'
 ---
 `;
 
-try {
-  fs.appendFileSync(markdownFileName, content);
-  console.log("Markdown file created.");
-} catch (err) {
-  console.error(err);
-}
-
-try {
-  if (!fs.existsSync(folderName)) {
-    fs.mkdirSync(folderName);
-    console.log("Asset folder created.");
+  try {
+    fs.appendFileSync(path, content);
+    console.log('Markdown file created.');
+  } catch (err) {
+    console.error(err);
   }
-} catch (err) {
-  console.error(err);
-}
 
-console.log("Finished!");
+  try {
+    fs.mkdirSync(`./public/${imagePath}`);
+  } catch (err) {
+    console.error(err);
+  }
+
+  console.log('Finished!');
+
+  function sluggify(title) {
+    // Remove leading/trailing whitespaces and convert to lowercase
+    let slug = title.trim().toLowerCase();
+
+    // Replace special characters with dashes
+    slug = slug.replace(/[^\w\s-]/g, '');
+
+    // Replace spaces with dashes
+    slug = slug.replace(/\s+/g, '-');
+
+    // Remove consecutive dashes
+    slug = slug.replace(/--+/g, '-');
+
+    // Return the sluggified title
+    return slug;
+  }
+})();
