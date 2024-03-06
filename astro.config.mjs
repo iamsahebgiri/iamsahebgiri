25687cfd63d24605df061fefc109107b7e3cd9a0
Saheb Giri <iamsahebgiri@gmail.com>
2024-03-06 21:10:23 +0530

    fix: known bugs and lh score

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,7 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
-import react from "@astrojs/react";
-
 export default defineConfig({
   site: 'https://iamsahebgiri.vercel.app',
-  integrations: [mdx(), sitemap(), tailwind(), react()]
+  integrations: [mdx(), sitemap(), tailwind()]
 });
